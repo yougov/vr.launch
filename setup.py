cec65b5b658fb582f31a9fffc41ab8223619004d
Jason R. Coombs <jaraco@jaraco.com>
2017-01-05 10:18:19 -0500

    Customize skeleton for YG conventions

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-# Project skeleton maintained at https://github.com/jaraco/skeleton
+# Project skeleton maintained at kiln://support/skeleton
+# based on https://github.com/jaraco/skeleton
 
 import io
 import sys
@@ -13,17 +14,20 @@ with io.open('README.rst', encoding='utf-8') as readme:
 needs_wheel = {'release', 'bdist_wheel', 'dists'}.intersection(sys.argv)
 wheel = ['wheel'] if needs_wheel else []
 
+group = ''
 name = 'skeleton'
+dashname = name.replace('.', '-')
 description = ''
+url_tmpl = 'https://yougov.kilnhg.com/Code/Repositories/{group}/{dashname}'
 
 params = dict(
 	name=name,
 	use_scm_version=True,
-	author="Jason R. Coombs",
-	author_email="jaraco@jaraco.com",
+	author="YouGov, Plc.",
+	author_email="dev@yougov.com",
 	description=description or name,
 	long_description=long_description,
-	url="https://github.com/jaraco/" + name,
+	url=url_tmpl.format(**locals()),
 	packages=setuptools.find_packages(),
 	include_package_data=True,
 	namespace_packages=name.split('.')[:-1],
