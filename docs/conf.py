f2c5807f0abadb190c91efdba3f2f42e58316b32
Jason R. Coombs <jaraco@jaraco.com>
2017-03-30 14:54:37 -0400

    Replace Github style package url resolver with fogbugz links. Add references to kiln and fogbugz. Add a couple of common in-house dependencies with docs hosted in devpi.

--- docs/conf.py
+++ docs/conf.py
@@ -14,11 +14,13 @@ link_files = {
         using=dict(
             GH='https://github.com',
             devpi='https://devpi.yougov.net/root/yg',
+            kiln='https://yougov.kilnhg.com/Code/Repositories',
+            fogbugz='https://yougov.fogbugz.com',
         ),
         replace=[
             dict(
-                pattern=r'(Issue )?#(?P<issue>\d+)',
-                url='{package_url}/issues/{issue}',
+                pattern=r'(Case |#)(?P<issue>\d+)',
+                url='{fogbugz}/f/cases/{issue}/',
             ),
             dict(
                 pattern=r'^(?m)((?P<scm_version>v?\d+(\.\d+){1,2}))\n[-=]+\n',
@@ -27,6 +29,14 @@ link_files = {
             dict(
                 pattern=r'PEP[- ](?P<pep_number>\d+)',
                 url='https://www.python.org/dev/peps/pep-{pep_number:0>4}/',
+            ),
+            dict(
+                pattern=r"queso (?P<queso_ver>\d+(\.\d+)*([abc]\d+)?)",
+                url='{devpi}/queso/latest/+doc/history.html',
+            ),
+            dict(
+                pattern=r"pan (?P<pan_ver>\d+(\.\d+)*([abc]\d+)?)",
+                url='{devpi}/pan/latest/+doc/history.html',
             ),
         ],
     ),
